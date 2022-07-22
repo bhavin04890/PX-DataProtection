fa217e61e0270eeec1fc789e406ae67991dad94b
bhavin04890 <bshah@purestorage.com>
2022-07-22 13:56:40 -0400

    updated cleanup.sh file

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,18 @@
 echo "Cleaning up the AWS Immersion Day labs" 
 
+if [ ! -f ~/usr/local/bin/eksctl ]; then
+	curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+	sudo mv /tmp/eksctl /usr/local/bin
+fi
+
+if [ ! -f ~/usr/local/bin/kubectl ]; then
+	curl --silent -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	chmod +x kubectl
+	mkdir -p ~/.local/bin
+	mv ./kubectl ~/usr/local/bin/
+	export PATH=$PATH:~/usr/local/bin/
+fi
+
 echo "Deleting the destination cluster"
 eksctl utils write-kubeconfig --cluster=px-dataprotection-destination
 kubectl delete -f k8s-logo.yaml -n demo
