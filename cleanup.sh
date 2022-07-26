2ae997aa1da897412783e0ac5169ce1341000d8d
bhavin04890 <bshah@purestorage.com>
2022-07-26 11:31:28 -0400

    Updating cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -14,7 +14,7 @@ if [ ! -f ~/usr/local/bin/kubectl ]; then
 fi
 
 echo "Deleting the destination cluster"
-eksctl utils write-kubeconfig --cluster=px-dataprotection-destination
+eksctl utils write-kubeconfig --cluster px-dataprotection-destination 
 kubectl delete -f k8s-logo.yaml -n demo
 sleep 30
 kubectl delete -f postgres.yaml -n demo 
@@ -22,14 +22,25 @@ sleep 15
 kubectl delete deploy stork -n kube-system 
 sleep 15 
 eksctl delete cluster -f eks-destination-cluster.yaml
-
-eksctl utils write-kubeconfig --cluster=px-dataprotection-source 
+ 
+eksctl utils write-kubeconfig --cluster px-dataprotection-source
 kubectl delete -f k8s-logo.yaml -n demo
 sleep 30
 kubectl delete -f postgres.yaml -n demo 
 sleep 15
 kubectl delete deploy stork -n kube-system 
 sleep 15 
+kubectl delete -f mongo-service.yaml -n pacman
+sleep 15 
+kubectl delete -f pacman-service.yaml -n pacman 
+sleep 15
+kubectl delete -f mongo-deployment.yaml -n pacman
+sleep 15 
+kubectl delete -f pacman-deployment.yaml -n pacman
+sleep 15 
+kubectl delete -f mongo-pvc.yaml -n pacman
+sleep 15
+
 eksctl delete cluster -f eks-source-cluster.yaml
 
 echo "Deleting Backup buckets"
