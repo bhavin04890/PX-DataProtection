4890a718f19ead93c9d3f1bb5e1c444f83eebc2f
bhavin04890 <bshah@purestorage.com>
2022-08-02 09:59:02 -0400

    Update pre-req.sh

--- pre-req.sh
+++ pre-req.sh
@@ -13,9 +13,8 @@ fi
 
 if [ ! -f ~/usr/local/bin/kubectl ]; then
 	echo " Step 2: Installing kubectl"
-	curl --silent -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-	chmod +x kubectl
-	mkdir -p ~/.local/bin
+        curl --silent -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
+        chmod +x kubectl
 	sudo cp ./kubectl /usr/local/bin/
 	export PATH=$PATH:/usr/local/bin/
 	echo "kubectl installed successfully with version:"
